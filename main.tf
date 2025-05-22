ac6bb8e9c2a3c83e071190743131c863cc2e8735
Vijay999712 <vijaybabu920@gmail.com>
2025-05-22 15:32:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.4"
 
-  cluster_name    = Softility.Cluster
+  cluster_name    = var.cluster_name
   cluster_version = "1.29"
   subnet_ids      = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
