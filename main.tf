21ce432fa699699d5f6d5ecf3cb7faf1682cbe52
Vijay999712 <vijaybabu920@gmail.com>
2025-05-22 18:54:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,27 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "delegate" {
-  source = "harness/harness-delegate/kubernetes"
-  version = "0.2.2"
-
-  account_id = "ucHySz2jQKKWQweZdXyCog"
-  delegate_token = "NTRhYTY0Mjg3NThkNjBiNjMzNzhjOGQyNjEwOTQyZjY="
-  delegate_name = "terraform-delegate"
-  deploy_mode = "KUBERNETES"
-  namespace = "harness-delegate-ng"
-  manager_endpoint = "https://app.harness.io"
-  delegate_image = "us-docker.pkg.dev/gar-prod-setup/harness-public/harness/delegate:25.05.85801"
-  replicas = 1
-  upgrader_enabled = true
-}
-
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.2"
@@ -74,6 +53,27 @@ module "eks" {
       AmazonEKS_CNI_Policy              = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
       AmazonEC2ContainerRegistryReadOnly = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
     }
+  }
+ }
+
+module "delegate" {
+  source = "harness/harness-delegate/kubernetes"
+  version = "0.2.2"
+
+  account_id = "ucHySz2jQKKWQweZdXyCog"
+  delegate_token = "NTRhYTY0Mjg3NThkNjBiNjMzNzhjOGQyNjEwOTQyZjY="
+  delegate_name = "terraform-delegate"
+  deploy_mode = "KUBERNETES"
+  namespace = "harness-delegate-ng"
+  manager_endpoint = "https://app.harness.io"
+  delegate_image = "us-docker.pkg.dev/gar-prod-setup/harness-public/harness/delegate:25.05.85801"
+  replicas = 1
+  upgrader_enabled = true
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
   }
  }
 }
