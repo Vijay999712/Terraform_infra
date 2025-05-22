5b549c862ed1f20ee5bc06f059658890009beb7c
Vijay999712 <vijaybabu920@gmail.com>
2025-05-22 18:44:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,27 @@ provider "aws" {
   region = "us-east-1"
 }
 
+module "delegate" {
+  source = "harness/harness-delegate/kubernetes"
+  version = "0.2.2"
+
+  account_id = "ucHySz2jQKKWQweZdXyCog"
+  delegate_token = "NTRhYTY0Mjg3NThkNjBiNjMzNzhjOGQyNjEwOTQyZjY="
+  delegate_name = "terraform-delegate"
+  deploy_mode = "KUBERNETES"
+  namespace = "harness-delegate-ng"
+  manager_endpoint = "https://app.harness.io"
+  delegate_image = "us-docker.pkg.dev/gar-prod-setup/harness-public/harness/delegate:25.05.85801"
+  replicas = 1
+  upgrader_enabled = true
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.2"
